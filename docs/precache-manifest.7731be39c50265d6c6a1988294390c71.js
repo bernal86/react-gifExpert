64a861cd120fbbd3eee7c0cd0a682f37b775278c
Andres Bernal <andres.bernal86@gmail.com>
2020-08-05 21:56:52 -0400

    carpeta docs agregado

---
+++ docs/precache-manifest.7731be39c50265d6c6a1988294390c71.js
@@ -0,0 +1,26 @@
+window.self.__precacheManifest = (window.self.__precacheManifest || []).concat([
+  {
+    "revision": "42d03c3a330e647e3e9ec38796f05775",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7f39763f3e737062df4",
+    "url": "/static/css/main.236ccdc9.chunk.css"
+  },
+  {
+    "revision": "c891655c70870ca4f1e5",
+    "url": "/static/js/2.00eec8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eec8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f39763f3e737062df4",
+    "url": "/static/js/main.8e805784.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
